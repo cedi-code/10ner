8127e0f91d9f3e0db7f397774d474f8b007576be
Leuenberger Florian <bleuef@bbc-be.local>
2018-04-17 11:39:37 +0200

    Unsere 10ner Datenbank

--- data/schema.sql
+++ data/schema.sql
@@ -10,3 +10,34 @@ CREATE TABLE  user (
 
 INSERT INTO user (firstName, lastName, email, password) VALUES ('Ramon',  'Binz',  'ramon.binz@bbcag.ch',   sha1('ramon'));
 INSERT INTO user (firstName, lastName, email, password) VALUES ('Samuel', 'Wicky', 'samuel.wicky@bbcag.ch', sha1('samuel'));
+
+//Unsere Datenbank
+DROP DATABASE 10ner_DB;
+CREATE DATABASE 10ner_DB;
+USE 10ner_DB;
+CREATE TABLE Benutzer (
+    ID_Ben integer primary key AUTO_INCREMENT,
+    Benutzername varchar(50),
+    Email varchar(50),
+    Passwort varchar(50)
+);
+CREATE TABLE Kategorie (
+    ID_Kat integer primary key AUTO_INCREMENT,
+    kategorie varchar(50)
+);
+CREATE TABLE Bild (
+   	ID_Bild integer primary key AUTO_INCREMENT,
+    Pfad varchar(50),
+    Inhaber_id integer,
+    Kategorie_id integer,
+    FOREIGN KEY (Inhaber_id) REFERENCES Benutzer(ID_Ben),
+    FOREIGN KEY (Kategorie_id) REFERENCES Kategorie(ID_Kat)
+);
+CREATE TABLE Bewertung (
+	ID_Bew integer primary key AUTO_INCREMENT,
+    Bild_id integer,
+    Bewerter_id integer,
+    Bewertung integer(10),
+    FOREIGN KEY (Bild_id) REFERENCES Bild(ID_Bild),
+    FOREIGN KEY (Bewerter_id) REFERENCES Benutzer(ID_Ben)
+);
