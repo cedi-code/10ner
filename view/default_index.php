ef3ea282aa756c19668f3331de6fba102fd91fa7
cedi-code <34920379+cedi-code@users.noreply.github.com>
2018-04-17 22:56:08 +0200

    Home erweiterung + Navigation
    Animationen in der Navigationen und das CSS file verbessert

--- view/default_index.php
+++ view/default_index.php
@@ -1,21 +1,35 @@
-<section class="intro" >
+<section class="intro homeBackground" >
     <div class="introBox">
         <div class="content">
             <div class="imgbox">
-                <img src="\images\camille.jpg"/>
+                <img src="bild.PNG"/>
             </div>
             <div class="textbox text-nowrap">
                 <h1>10ner</h1>
                 <h3>Superficial and obnoxious</h3>
 
             </div>
-            <input type="range" min="0" max="10" value="0" class="range"/>
+            <input type="range" min="0" max="10" value="10" class="range"/>
             <p style="padding-top: 10px; display: inline-block" class="bottom">Learn more about the Philosophy from 10ner</p>
 
-            <div class="text-center navbar-fixed-bottom">
+            <div class="text-center navbar-fixed-bottom" style="padding-bottom: 10%">
                 <h1>&#x21e9;</h1>
             </div>
 
+        </div>
+    </div>
+</section>
+<section class="intro intro-70" >
+    <div class="introBox">
+        <div style="color:#000;" class="indexContentOr ">
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <span class="float-md-left"><div class="textbox text-nowrap"><h3 class="m-3">Rate people by there Looks!</h3></div><img src="bild.PNG"/></span>
+                </div>
+                <div class="col-md-6 text-center">
+                    <span class="float-md-right"><div class="textbox text-nowrap"><h3 class="m-3">Explore other genres</h3></div><img src="bild.PNG"/></span>
+                </div>
+            </div>
         </div>
     </div>
 </section>
