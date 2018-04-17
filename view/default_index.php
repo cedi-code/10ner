8f51687378c104e21cfe6ce900793b51f3a82bc4
cedi-code <34920379+cedi-code@users.noreply.github.com>
2018-04-17 16:49:28 +0200

    more CSS
    nire CSS

--- view/default_index.php
+++ view/default_index.php
@@ -7,8 +7,11 @@
             <div class="textbox text-nowrap">
                 <h1>10ner</h1>
                 <h3>Superficial and obnoxious</h3>
-                <p style="padding-top: 10px; display: inline-block" class="bottom">Learn more about the Philosophy from 10ner</p>
+
             </div>
+            <input type="range" min="0" max="10" value="0" class="range"/>
+            <p style="padding-top: 10px; display: inline-block" class="bottom">Learn more about the Philosophy from 10ner</p>
+
             <div class="text-center navbar-fixed-bottom">
                 <h1>&#x21e9;</h1>
             </div>
