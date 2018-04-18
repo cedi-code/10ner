8a8bd11d500dab1281c21c4f0f283e18d1362922
cedi-code <34920379+cedi-code@users.noreply.github.com>
2018-04-18 21:40:24 +0200

    default_view slider

--- view/default_index.php
+++ view/default_index.php
@@ -1,17 +1,21 @@
 <section class="intro homeBackground" >
     <div class="introBox">
-        <div class="content">
+        <div class="content animated fadeInUp">
             <div class="imgbox">
                 <img src="images/camille.jpg"/>
             </div>
+            <div class="animated fadeInLeft animation-delay-075">
             <div class="textbox text-nowrap">
                 <h1>10ner</h1>
                 <h3>Superficial and obnoxious</h3>
 
             </div>
-            <input type="range" min="0" max="10" value="10" class="range"/>
+            <table class="range-value">
+                <td id="range1">1</td ><td id="range2">2</td><td id="range3">3</td><td id="range4">4</td><td id="range5">5</td><td id="range6">6</td><td id="range7">7</td><td id="range8">8</td><td id="range9">9</td><td id="range10">10</td>
+            </table>
+            <input id="range"type="range" min="1" max="10" value="10" class="range"/>
             <p style="padding-top: 10px; display: inline-block" class="bottom">Learn more about the Philosophy from 10ner</p>
-            
+            </div>
 
         </div>
     </div>
@@ -24,7 +28,7 @@
                     <span class="float-md-left "><div class="textbox text-nowrap"><h3 class="m-3">Rate people by there Looks!</h3></div><img class="img-w-70" src="images/otherBild.jpg"/></span>
                 </div>
                 <div class="col-md-6 text-center">
-                    <span class="float-md-right"><div class="textbox text-nowrap"><h3 class="m-3">Explore other genres</h3></div><img class="img-w-70" src="images/cute_pomeranian_dog.jpg"/></span>
+                    <span class="float-md-right "><div class="textbox text-nowrap"><h3 class="m-3">Explore other genres</h3></div><img class="img-w-70" src="images/cute_pomeranian_dog.jpg"/></span>
                 </div>
             </div>
         </div>
