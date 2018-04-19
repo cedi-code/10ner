83cd7e19a2ee4acd692d1e30da72b7d14bb6ab22
cedi-code <34920379+cedi-code@users.noreply.github.com>
2018-04-19 22:09:03 +0200

    Rate View Css and Design
    Need to add the Submit button!

--- view/rateView.php
+++ view/rateView.php
@@ -1,11 +1,14 @@
-<section class="intro" >
+<section class="intro homeBackground" >
     <div class="introBox">
-        <div class="content">
-            <?php
-                echo $bildPfad;
-                echo "<img src=\"$bildPfad\"/>";
-            ?>
-
+        <div class="contentRate">
+            <div class="rateImage" style="background-image: url('images/camille.jpg')"></div>
+            <table class="range-value">
+                <td id="range1">1</td ><td id="range2">2</td><td id="range3">3</td><td id="range4">4</td><td id="range5">5</td><td id="range6">6</td><td id="range7">7</td><td id="range8">8</td><td id="range9">9</td><td id="range10">10</td>
+            </table>
+            <form action="/rate/index">
+            <input id="range"type="range" min="1" max="10" value="5" name="bewertung"class="range"/>
+            <!-- todo echo a submit button + echo input normaly -->
+            </form>
         </div>
     </div>
 </section>
