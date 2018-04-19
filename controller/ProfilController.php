c1ed5496a9566dabc59eb4b3fcabaa96cc8ce948
cedi-code <34920379+cedi-code@users.noreply.github.com>
2018-04-19 08:51:57 +0200

    user_edit + login handler

---
+++ controller/ProfilController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bgirac
+ * Date: 19.04.2018
+ * Time: 08:17
+ */
+
+class ProfilController
+{
+    public function index()
+    {
+        // In diesem Fall möchten wir dem Benutzer die View mit dem Namen
+        //   "default_index" rendern. Wie das genau funktioniert, ist in der
+        //   View Klasse beschrieben.
+        @session_start();
+        if(isset($_SESSION['uid'])) {
+            $view = new View('user_edit');
+            $view->title =  $_SESSION['benutzername'];
+            $view->heading = $_SESSION['benutzername'];
+            $view->display();
+        }else {
+            header("Location: /");
+        }
+
+    }
+}
---
+++ view/user_edit.php
@@ -0,0 +1,13 @@
+<section class="intro" >
+    <div class="introBox">
+        <div class="content colorMainBlue">
+            <h2>Register</h2>
+            <?php
+                echo "<div class=\"imgbox\">
+                        <img src=\"images/camille.jpg\"/>
+                        </div>";
+                echo "<h2>" . $_SESSION['benutzername'] . "</h2>";
+            ?>
+        </div>
+    </div>
+</section>
