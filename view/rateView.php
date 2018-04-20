ff86fa100375350df8b7cdff03cea62f37a2ad08
Girardin Cedric <bgirac@bbc-be.local>
2018-04-20 08:12:33 +0200

    RateView added submit button

--- view/rateView.php
+++ view/rateView.php
@@ -5,10 +5,13 @@
             <table class="range-value">
                 <td id="range1">1</td ><td id="range2">2</td><td id="range3">3</td><td id="range4">4</td><td id="range5">5</td><td id="range6">6</td><td id="range7">7</td><td id="range8">8</td><td id="range9">9</td><td id="range10">10</td>
             </table>
-            <form action="/rate/index">
-            <input id="range"type="range" min="1" max="10" value="5" name="bewertung"class="range"/>
-            <!-- todo echo a submit button + echo input normaly -->
-            </form>
+            <?php
+                $form = new Form('/rate/index', 'POST',  'multipart/form-data');
+                echo "<input id=\"range\"type=\"range\" min=\"1\" max=\"10\" value=\"5\" name=\"bewertung\" class=\"range\"/>";
+                echo  $form->submit()->label('bestätigen')->name('submitRate');
+                $form->end();
+            ?>
+
         </div>
     </div>
 </section>
